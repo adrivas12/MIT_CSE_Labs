ff57d22c81b97ccc9f98c97cda6d429f9444b78c
Aditi Shrivastava <88084673+adrivas12@users.noreply.github.com>
2023-08-22 15:49:27 +0530

    Update aq2.c

--- Semester_5/OS_lab/lab2/aq2.c
+++ Semester_5/OS_lab/lab2/aq2.c
@@ -2,20 +2,53 @@
 working directory. The name of the file should appear followed by a prompt as to
 whether it should be removed.*/
 
-#include <sys/types.h>
-#include <dirent.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dirent.h>
 
-void main()
+int main(int argc, char* argv[])
 {
-	struct dirent **entry;
-	struct stat statbuf; 
-	char file[];
-	printf("Enter the file you want to delete: \n");
-	scanf("%s", file); 
+	if(argc != 2)
+	{
+		printf("\nWrong number of arguments");
+		exit(1);
+	}
 	
+	DIR* directory = opendir(argv[1]);
+	struct stat statbuf;
+	struct dirent* entry;
+	int option = 0;
+	
+	if(directory == NULL)
+	{
+		printf("\nCould not open directory");
+		exit(1);
+	}
+	
+	chdir(argv[1]);
+	
+	while((entry = readdir(directory)) != NULL)
+	{
+		lstat(entry->d_name, &statbuf);
+		if(!S_ISDIR(statbuf.st_mode))
+		{
+			printf("\nDo you want to remove %s [1/0]: ", entry->d_name);
+			scanf("%d", &option);
+			if(!option)
+				continue;
+			else
+			{
+				char path[256];
+				strcpy(path, argv[1]);
+				strcat(path, "/");
+				strcat(path, entry->d_name);
+				printf("\n%s", path);
+				remove(path);
+			}	
+		}
+	}
 }
