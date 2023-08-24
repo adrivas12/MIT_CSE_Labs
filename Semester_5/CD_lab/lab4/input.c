7dc591f674b2a2050929dc4b439edb513934452e
Aditi Shrivastava <88084673+adrivas12@users.noreply.github.com>
2023-08-24 16:26:36 +0530

    Add files via upload

---
+++ Semester_5/CD_lab/lab4/input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int suming(int a, int b)
+{
+    int sum; 
+    sum=a+b; 
+    return sum; 
+}
+int main()
+{
+    int a; 
+    int b; 
+    suming(a, b); 
+    printf("Hello World");
+}
