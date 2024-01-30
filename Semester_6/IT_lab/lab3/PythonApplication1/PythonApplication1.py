01da9ff8ac778a9eb2f0020a038f9850cbf8ad44
Aditi Shrivastava <88084673+adrivas12@users.noreply.github.com>
2024-01-30 10:22:26 +0530

    Add files via upload

---
+++ Semester_6/IT_lab/lab3/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,7 @@
+#Write a python program to implement simple calculator which 
+#perform addition, substraction, multiplication and division
+
+a = input("Enter input string: ")
+print(a)
+res= eval(a)
+print(res)
---
+++ Semester_6/IT_lab/lab3/PythonApplication1/q4.py
@@ -0,0 +1,7 @@
+#Write a python program to sort words in alphabetical order
+
+str = input("Enter words: ")
+S= str.split(" ")
+W=sorted(S)
+print(' '.join(W))
+
